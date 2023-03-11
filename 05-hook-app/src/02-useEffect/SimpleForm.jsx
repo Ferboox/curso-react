9b5771f9e12a85c4469c195541f48fd2c8398299
ferbargar99@hotmail.com <ferbargar99@hotmail.com>
2023-03-11 16:14:45 -0600

    05-Hooks

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: "Ferboox",
+        email: "fernando@outlook.com",
+    });
+
+    const { username, email } = formState;
+
+
+    const onInputChange = ({target}) => {
+
+        const {name, value} = target;
+
+        setFormState({
+            ...formState,
+            [ name ]: value
+        }, []);
+    }
+
+    useEffect( () => {
+        console.log('object');
+    }, [formState]);
+
+
+    return (
+        <>
+            <h1>Formulario</h1>
+            <hr />
+
+            <input type="text"  className="form-control" placeholder="Username" name="username"  onChange={onInputChange} value={username}/>
+
+            <input type="email"  className="form-control mt-2" placeholder="Email" name="email" onChange={onInputChange} value={email} />
+
+        </>
+    )
+}
